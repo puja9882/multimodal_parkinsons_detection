42daef4355be3735f52c005195d35ade26be20b6
puja9882 <waykolepuja@gmail.com>
2026-03-16 19:09:45 +0530

    Update server.py

--- data/src/api/server.py
+++ data/src/api/server.py
@@ -533,5 +533,4 @@ init_db()
 
 # ================= RUN =================
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port, debug=False)
+    app.run(host="0.0.0.0", port=10000)
