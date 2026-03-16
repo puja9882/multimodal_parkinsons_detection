f0f0ebd2f67ed35ec1742c1427c60fe2f7ae4d86
puja9882 <waykolepuja@gmail.com>
2026-03-16 19:42:56 +0530

    Update server.py

--- data/src/api/server.py
+++ data/src/api/server.py
@@ -350,9 +350,10 @@ def predict():
             voice_file.save(voice_path)
             temp_files.append(voice_path)
 
-        d_prob = float(drawing_model.predict(
-            get_drawing_input(spiral_path), verbose=0
-        ).flatten()[0])
+        img_input = get_drawing_input(spiral_path, drawing_model)
+        d_prob = float(
+            drawing_model.predict(img_input, verbose=0).flatten()[0]
+        )
 
         feature_df = extract_voice_from_wav(voice_path, 65.0)
 
